bca7efbc96ff8e40a587897928a1c015e9e64a12
Nayan Shrees <nayanYshrees@gmail.com>
2021-08-15 03:58:44 +0100

    completed chapter 3 section on functions

--- Chapter 3/functions/src/main.rs
+++ Chapter 3/functions/src/main.rs
@@ -1,8 +1,40 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function(5, 6);
+
+    //Expressions and statements
+    //Statements do not return values
+    //Expressions evaluate to a resulting value
+    //Rust is an expression-based language
+    //Expressions don't end in semi colons otherwise they would be statements
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of x is {}, y is {}", x, y);
+
+    let x = five();
+    println!("The value of x is {}", x);
+
+    let x = plus_one(x);
+    println!("The value of x is {}", x);
+    
 }
 
-fn another_function() {
-    println!("Another function.");
+//In functions parameters the type must be declared 
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is {}.", x);
+    println!("The value of y is {}.", y);
+}
+
+//Return values have to have their type declared using an arrow ->
+//You can return from a function early using the return keyword and specifying a value
+//Most functions return the last expression implicitly
+fn five() -> i32{
+    5
+}
+
+fn plus_one(x: i32) -> i32{
+    return x + 1;
 }
